541d7b640f0967193eb7cecceb605375e7bbcb77
Hiraq <hrxwan@gmail.com>
2012-03-23 20:18:39 +0700

    item lookup

--- Amazon/Request/Lookup.php
+++ Amazon/Request/Lookup.php
@@ -2,6 +2,12 @@
 
 namespace AmazonProductAdvertising\Amazon\Request;
 
+/*
+ * import and aliasing data for :
+ * - Base
+ */
+use AmazonProductAdvertising\Amazon\Request\Base as Amazon_Request_Base;
+
 /**
  * @author Hiraq
  * @link https://github.com/hiraq/php-amazon-pa-wrapper
@@ -11,6 +17,56 @@ namespace AmazonProductAdvertising\Amazon\Request;
  * @version 1.0
  * @final
  */
-final class Lookup {
+final class Lookup extends Amazon_Request_Base {
+    
+    /**
+     *
+     * Search Object
+     * @staticvar null|object
+     */
+    static private $_obj = null;            
+    
+    /**
+     * Denied direct object instantiation
+     * 
+     * @access private
+     * @return void 
+     */
+    private function __construct() {}
+    
+    /**
+     *
+     * Create Lookup object
+     * 
+     * @access public
+     * @return Search
+     * @static
+     */
+    static public function getInstance() {
+        
+        if( is_null(self::$_obj) ) {
+            self::$_obj = new Lookup();
+        }
+        
+        return self::$_obj;
+        
+    }                        
+    
+    /**
+     *
+     * Get signatured request
+     * 
+     * @access public
+     * @uses Amazon_Signature
+     * @return string     
+     */
+    public function build() {                
+        
+        return $this->_signature->create(array(
+            'ItemId' => $this->_data,
+            'Operation' => 'ItemLookup'
+        ));
+        
+    }
     
 }
